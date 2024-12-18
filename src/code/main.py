a46d6e6e3e5d2755ed3ee2db2346e0075c94ad75
Danbackal <danbackal@gmail.com>
2024-12-18 15:35:18 -0700

    starting code I wrote. Not happy with all of it yet. Trying to just get the snake head to move

---
+++ src/code/main.py
@@ -0,0 +1,118 @@
+import pygame
+from pygame.locals import *
+from random import randint
+
+from snake import Snake
+
+# Snake Game
+# Need game class - hold creation of apples, holds score, holds snake.
+# Snake class - do we need a full class? Or can we create a snake segment class
+# and game just holds and updates the snake segment group?
+# Initial thought - set up game holding everything. May not need multiple classes this time.
+
+# Playable area will be 400x400. Extra 100 on top for title/score
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 500
+FPS = 30
+
+
+class Game:  # Game does not need to be a sprite - it holds sprites.
+    def __init__(self, screen_width, screen_height):
+        # Start with game information
+        self.game_start = 0
+        self.game_running = 1
+        self.game_paused = 2
+        self.game_end = 3
+        self.game_close = 4
+        self.game_state = 1  # Will change when I build the menus
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+
+        # defining sizes as variable, in case I want to change the size of the game later
+        # defining board borders for collision and bounding
+        self.board_top = (screen_height/5) + 4
+        self.board_bottom = screen_height - 4
+        self.board_left = 4
+        self.board_right = screen_width - 4
+
+        # defining pixel height for conversion - a square so don't need x and y
+        self.pixel_size = 4
+
+        self.header = pygame.rect.Rect(0, 0, screen_width, screen_height)
+        self.board = pygame.rect.Rect(self.board_left, self.board_top,
+                                      self.board_right - self.board_left, self.board_bottom - self.board_top)
+
+        # Game Pieces
+        self.snake = pygame.sprite.Group()
+        self.snake_head = Snake(self, (((self.board_right - self.board_left) / 2) + self.pixel_size / 2,
+                                       (self.board_top + (self.board_bottom - self.board_top) / 2)
+                                       - self.pixel_size / 2))
+        self.snake.add(self.snake_head)
+        # Will need to make this a function that chooses a random place on the board not currently occupied by snake
+        # possibly eventually an "available spaces" type list, and choose a random
+        # list and index from the size available
+        self.apple = pygame.rect.Rect(randint(self.board_left, self.board_right),
+                                      randint(self.board_top, self.board_bottom - self.pixel_size),
+                                      self.pixel_size, self.pixel_size)
+
+    def update(self, event):
+        match self.game_state:
+            # I shouldn't have button pressing problem when I pull the latest event each time
+            case self.game_start:
+                print()
+            case self.game_running:
+                pressed_keys = pygame.key.get_pressed()
+                if pressed_keys[K_p]:
+                    print("Pause here")
+                else:
+                    # Will add here, if collision with apple, then ate. False for testing
+                    self.snake_head.update(pressed_keys, False)
+                    print()
+            case self.game_paused:
+                print()
+            case self.game_end:
+                print()
+            case self.game_close:
+                print("Game Closing")
+
+    def draw(self, surface):
+        # Draw header section
+        pygame.draw.rect(surface, "grey", self.header)
+
+        # Draw board section
+        pygame.draw.rect(surface, "black", self.board)
+        pygame.draw.rect(surface, "white", self.apple)
+        self.snake.draw(surface)
+
+    def run_game(self):
+        return self.game_state != self.game_close
+
+    def close_game(self):
+        self.game_state = self.game_close
+
+    def get_pixel_size(self):
+        return self.pixel_size
+
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Snake")
+clock = pygame.time.Clock()
+_game = Game(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+# Game loop
+while _game.run_game():
+    # check for closures
+    # Need a way to say if this is a mouse click, pass it to update also
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            _game.close_game()
+
+    _game.update("Pass")
+
+    # draw
+    _game.draw(screen)
+    pygame.display.update()
+    clock.tick(FPS)
+
+pygame.quit()
---
+++ src/code/snake.py
@@ -0,0 +1,77 @@
+import pygame
+from pygame.locals import *
+
+
+# Snake class really just implements a body section of the snake.
+# This allows us to iterate through a list, and tell the next segment where to move
+# It also allows us to keep a snake head
+# It will function like a linked list, where the node value is the rectangle, and next is the next segment
+class Snake(pygame.sprite.Sprite):
+    def __init__(self, game, location):
+        super().__init__()
+        self.game = game
+        self.body_size = game.get_pixel_size()
+        # Will need to add to this. Head will get a value.
+        # When food is eaten, tail will add one in its position
+        self.image = pygame.surface.Surface((self.body_size, self.body_size))
+        self.rect = self.image.get_rect(center=location)
+        self.image.fill("green")
+        self.next = None
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+    def update(self, new_location, ate):
+        # Get current location to update next leg with
+        location = self.rect.center
+        # Set current leg to new location
+        self.rect.move_ip(new_location[0] - self.rect.x, new_location[1] - self.rect.y)
+
+        # If there is more snake, continue the trend
+        if self.next is not None:
+            return self.next.update(location, ate)
+        elif ate:
+            new_snake = Snake(self.game, location)
+        else:
+            new_snake = None
+        # If we added a new snake, we need to pass it back to the game update, to add to group
+        self.next = new_snake
+        return new_snake
+
+
+class SnakeHead(Snake):
+    # All the current init info is the same, really just want a unique update method
+    def __init__(self, game, location):
+        super().__init__(game, location)
+        self.up = 0
+        self.right = 1
+        self.down = 2
+        self.left = 3
+        self.direction = 4
+
+    def update(self, pressed, ate):
+        # keys pressed matters here
+        location = self.rect.center
+        if pressed[K_UP]:
+            if self.direction != self.down:
+                self.direction = 0
+        if pressed[K_RIGHT]:
+            if self.direction != self.left:
+                self.direction = 1
+        if pressed[K_DOWN]:
+            if self.direction != self.up:
+                self.direction = 2
+        if pressed[K_LEFT]:
+            if self.direction != self.right:
+                self.direction = 3
+        match self.direction:
+            case self.up:
+                self.rect.move_ip(0, -self.body_size)
+            case self.right:
+                self.rect.move_ip(self.body_size, 0)
+            case self.down:
+                self.rect.move_ip(0, self.body_size)
+            case self.left:
+                self.rect.move_ip(-self.body_size, 0)
+        # return self.next.update(location, ate)
+
